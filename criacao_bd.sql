8d10e2af69e031e14c276fe78a81f99aaf9d0681
denini08 <denini.silva@upe.br>
2018-09-04 08:31:26 -0300

    Alteracao BD definitivo

--- criacao_bd.sql
+++ criacao_bd.sql
@@ -2,13 +2,15 @@ create database tcc;
 
 CREATE TABLE Trabalhos (
   id int(11) NOT NULL AUTO_INCREMENT,
-  isbn varchar(13) NOT NULL,
+  isbn varchar(13) NOT NULL UNIQUE,
   titulo varchar(150) DEFAULT NULL,
   tema varchar(150) DEFAULT NULL,
   autor varchar(100) DEFAULT NULL,
   curso varchar(100) DEFAULT NULL,
   ano int(11) DEFAULT NULL,
   semestre int(11) DEFAULT NULL,
+  pdf varchar(100) DEFAULT NULL,
   orientadores varchar(150) DEFAULT NULL,
+
   PRIMARY KEY (id)
 );
